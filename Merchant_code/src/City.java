fe00e4a4163f1b82499e549a21256e4175afd918
Alena <alenabelovadmvn@gmail.com>
2023-09-22 12:25:00 -0400

    City added

---
+++ Merchant_code/src/City.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class City {
+    String name;
+
+    public City(String name){
+        ArrayList<String> Fruits = new ArrayList<String>();
+        Fruits.add("Apple");
+        Fruits.add("Coconut");
+        Fruits.add("Orange");
+        Fruits.add("Berry");
+        Fruits.add("Peach");
+        System.out.println(Fruits);
+
+
+        ArrayList<Double> Price = new ArrayList<Double>();
+        Price.add(1.5);
+        Price.add(4.0);
+        Price.add(2.25);
+        Price.add(6.75);
+        Price.add(5.0);
+        System.out.println(Price);
+
+        ArrayList<Integer> Quantity = new ArrayList<Integer>();
+        Quantity.add(3);
+        Quantity.add(2);
+        Quantity.add(1);
+        Quantity.add(4);
+        Quantity.add(4);
+        System.out.println(Quantity);
+
+        //this one we have to have a object name game to put them all togheter
+        ArrayList<String> City = new ArrayList<>();
+        City.add("Montreal");
+        City.add("London");
+        City.add("Tokyo");
+        City.add("NewYork");
+
+
+
+    }
+}
