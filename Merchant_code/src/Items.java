a2763629e66c05fde001d495730b4609a62c0bc2
Alena <alenabelovadmvn@gmail.com>
2023-09-22 12:25:49 -0400

    Items class added

---
+++ Merchant_code/src/Items.java
@@ -0,0 +1,14 @@
+public class Items {
+    int price;
+    String name;
+//adding constructor //setter and getter //arrey list
+
+
+
+    public Items( int price,String name){
+
+        this.price = price;
+        this.name = name;
+
+    }
+}
