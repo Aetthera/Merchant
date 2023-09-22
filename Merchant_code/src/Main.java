f34277f793c9e823a9f991c5a068537422d5c82d
Alena <alenabelovadmvn@gmail.com>
2023-09-22 17:07:53 -0400

    Main updated with Player_Menu object

--- Merchant_code/src/Main.java
+++ Merchant_code/src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        // Create an instance of GameApp to start the game
+        Player_Menu gameApp = new Player_Menu();
 
+        // Initialize and run the game loop or user interface
+        gameApp.RunGame();
     }
 }
