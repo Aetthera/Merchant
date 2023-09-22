d5c299f2fffbfbc2562f97c95d5ef10fb6a6cc9b
Alena <alenabelovadmvn@gmail.com>
2023-09-22 12:26:25 -0400

    Class Smuggler added

---
+++ Merchant_code/src/Smuggler.java
@@ -0,0 +1,12 @@
+public class Smuggler {
+    double Money;
+    String Inventory;
+
+    public Smuggler( double Money, String Inventory){
+
+        this.Money = Money;
+        this.Inventory = Inventory;
+
+
+    }
+}
